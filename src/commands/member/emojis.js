7baf6aca5e3138312ae6708c6819bc6137e89339
Ian <ian.wiedenman@gmail.com>
2025-02-19 21:46:56 -0800

    Update emoji message

--- src/commands/member/emojis.js
+++ src/commands/member/emojis.js
@@ -25,18 +25,21 @@ module.exports = {
     const emojis = interaction.guild.emojis.cache.map(emoji => `${emoji} \`${emoji}\``);
     const chunk = 20;
 
-    // Log emojis
-    // assistant.log('Emojis in the server:\n', emojis);
-
     // Split the emojis into chunks of 10 emojis and send them
     for (let i = 0; i < emojis.length; i += chunk) {
       const current = emojis.slice(i, i + chunk);
-      const string = `${i === 0 ? '**Emojis**:\n' : ''}${current.join('\n')}`;
-      // Send to current channel
-      await interaction.channel.send(string);
-    }
+      const isFirst = i === 0;
+      const item = `${isFirst ? '**Emojis**:\n' : ''}${current.join('\n')}`;
 
-    // Return
-    return;
+      // Send to current channel
+      if (isFirst) {
+        await interaction.editReply({
+          content: item,
+          embeds: [],
+        });
+      } else {
+        await interaction.channel.send(item);
+      }
+    }
 	},
 };
